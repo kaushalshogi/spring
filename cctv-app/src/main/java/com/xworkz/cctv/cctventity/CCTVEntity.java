890a364660c5cd6f8e444c6e88a250bc4c6f3371
JayanthPrasad <jayasup@gmail.com>
2021-11-25 19:21:49 +0530

    dpring

---
+++ cctv-app/src/main/java/com/xworkz/cctv/cctventity/CCTVEntity.java
@@ -0,0 +1,47 @@
+package com.xworkz.cctv.cctventity;
+
+public class CCTVEntity {
+	private String brand;
+	private String color;
+	private double prize;
+	private int cameraSize;
+
+	public CCTVEntity() {
+		super();
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public double getPrize() {
+		return prize;
+	}
+
+	public void setPrize(double prize) {
+		this.prize = prize;
+	}
+
+	public int getCameraSize() {
+		return cameraSize;
+	}
+
+	public void setCameraSize(int cameraSize) {
+		this.cameraSize = cameraSize;
+	}
+	
+	
+
+}
